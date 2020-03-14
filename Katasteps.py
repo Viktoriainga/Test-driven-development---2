8cdcccbd59816064dd05e2143899bc0f10b12e0f
Viktoria Inga <viktoriainga@gmail.com>
2020-03-14 20:11:08 +0000

    add supports single digits

--- Katasteps.py
+++ Katasteps.py
@@ -1,3 +1,5 @@
 def add(number_str):
     if number_str == "":
         return 0
+    
+    return int(number_str)
--- Katasteps_test.py
+++ Katasteps_test.py
@@ -1,3 +1,7 @@
 import Katasteps
+
 def test_on_empty_string():
     assert Katasteps.add("") == 0
+
+def test_on_single_digit():
+    assert Katasteps.add("5") == 5
