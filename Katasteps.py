30f947bb3814d4f78989cdcf02fb9799b13774e3
Viktoria Inga <viktoriainga@gmail.com>
2020-03-25 17:24:54 +0000

    Negatives not allowed works

--- Katasteps.py
+++ Katasteps.py
@@ -1,14 +1,23 @@
+class NegativesNotAllowed(Exception):
+    pass
+
 def add(number_str):
     if number_str == "":
         return 0
 
     value = 0
-
+    negatives = ""
     for i in number_str.split(","):
         i= i.strip().split("\n")    
         for n in i:
+            if int(n) < 0:
+                negatives += str(n) + ","
+
             if int(n) > 1000:
                 n = "0"
+                
             value += int(n)
-
+    
+    if negatives != "":
+        raise NegativesNotAllowed("Negatives not allowed: {}".format(negatives[0:-1]))
     return value
--- Katasteps_test.py
+++ Katasteps_test.py
@@ -1,4 +1,7 @@
 import Katasteps
+from Katasteps import NegativesNotAllowed
+import pytest
+
 
 def test_on_empty_string():
     assert Katasteps.add("") == 0
@@ -17,3 +20,8 @@ def test_newlines():
 
 def test_on_numbers_bigger_than_1000():
     assert Katasteps.add("1001,2") == 2
+
+def testing_negatives():
+    with pytest.raises(NegativesNotAllowed) as excinfo:
+        Katasteps.add("-2,-3")
+    assert "Negatives not allowed" in str(excinfo.value)
